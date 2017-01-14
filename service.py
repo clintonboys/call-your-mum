20c8f4b490ec99ecaf9a5badc81357c75df269c8
Clinton Boys <cdsboys@gmail.com>
2017-01-14 14:06:25 +0200

    ready for first deploy

--- service.py
+++ service.py
@@ -10,8 +10,12 @@ import random
 from sendgrid.helpers.mail import *
 
 
-def send_message(timestamp, person):
+def send_message(timestamp, person, birthday = False):
 	sg = sendgrid.SendGridAPIClient(apikey = sendgrid_key)
+	if birthday:
+		birthday_message = "; it'\s their birthday!"
+	else:
+		birthday_message = ""
 	data = { "personalizations": [
 	    {
 	      "to": [
@@ -19,11 +23,11 @@ def send_message(timestamp, person):
 	          "email": "cdsboys@gmail.com"
 	        }
 	      ],
-	      "subject": "Get in touch with {0}".format(person._name)
+	      "subject": "Get in touch with {0}".format(person._name) + birthday_message
 	    }
 	  ],
 	  "from": {
-	    "email": "cdsboys@gmail.com"
+	    "email": "cdsboys@gmail.com",
 	  },
 	  "content": [
 	    {
@@ -48,7 +52,6 @@ def main():
 	hour_local = hour + local_time
 
 	if hour_local >= min_contact_hour and hour_local <= max_contact_hour:
-		#print hour_local
 
 		for person in people_list:
 			X = np.random.uniform(0,30*24*(60/service_frequency)/person.freq())
@@ -62,10 +65,10 @@ def main():
 					send_message(current_time, person)
 					person._last_contact = current_time
 			elif person.is_birthday() and (current_time - person.last_contact()).days > 0:
-				send_message(current_time, person)
+				send_message(current_time, person, True)
 				person._last_contact = current_time
 
 	save_people(people_list, 'people.json')
 
-for i in range(0,6000):
+if __name__ == '__main__':
 	main()
